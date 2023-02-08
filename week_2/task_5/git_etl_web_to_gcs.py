69d0f51542cedcf3527b9f9b2d81e5619e3e0d39
Zhanbulat <zhanik.mul@gmail.com>
2023-02-09 00:17:24 +0600

    Create git_etl_web_to_gcs.py

---
+++ week_2/task_5/git_etl_web_to_gcs.py
@@ -0,0 +1,15 @@
+from prefect.deployments import Deployment
+from prefect.filesystems import GitHub
+from etl_web_to_gcs import etl_web_to_gcs
+
+
+storage = GitHub.load("zoomcamp-question-4")
+
+deployment = Deployment.build_from_flow(
+     flow=etl_web_to_gcs,
+     name="q5_flow",
+     storage=storage,
+     entrypoint="week_2/task_5/etl_web_to_gcs.py:etl_web_to_gcs")
+
+if __name__ == "__main__":
+    deployment.apply()
